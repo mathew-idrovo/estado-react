125701034773cb264a9ced74166724c811a031e7
Mathew <mateoicecua2000@gmail.com>
2024-11-22 18:09:14 -0500

    Preparando para subir proyecto a GitHub

---
+++ src/classState.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class ClassState extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2> Eliminar UseState</h2>
+        <p>Por favor, escribe el codigo de seguridad</p>
+
+        <input placeholder="codigo de seguridad" />
+        <button>Comprobar</button>
+      </div>
+    );
+  }
+}
+
+export { ClassState };
---
+++ src/useState.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function UseState() {
+  return (
+    <div>
+      <h2> Eliminar UseState</h2>
+      <p>Por favor, escribe el codigo de seguridad</p>
+
+      <input placeholder="codigo de seguridad" />
+      <button>Comprobar</button>
+    </div>
+  );
+}
