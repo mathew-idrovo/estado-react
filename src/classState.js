48da313be07e740fe3bbd917acdc0b3a6f4083d9
Mathew <mateoicecua2000@gmail.com>
2024-12-04 17:12:10 -0500

    poner confiracion

--- src/classState.js
+++ src/classState.js
@@ -6,7 +6,7 @@ class ClassState extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      error: true,
+      error: false,
       value: "",
       loading: false,
     };
--- src/useState.js
+++ src/useState.js
@@ -6,38 +6,115 @@ export default function UseState({ name }) {
   const [value, setValue] = useState("");
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [state, setState] = useState({
+    value: "",
+    error: false,
+    loading: false,
+    deleted: false,
+    confirmed: false,
+  });
 
   useEffect(() => {
     console.log("Empezando efecto");
 
-    if (!!loading) {
+    if (!!state.loading) {
       setTimeout(() => {
         console.log("Haciendo validacion");
         if (value === SECURITY_CODE) {
-          setLoading(false);
+          setState({
+            ...state,
+            error: false,
+            loading: false,
+            confirmed: true,
+          });
         } else {
-          setError(true);
-          setLoading(false);
+          setState({
+            ...state,
+            error: true,
+            loading: false,
+          });
         }
         console.log("Terminando Validacion");
       }, 3000);
     }
-  }, [loading]);
+  }, [state.loading]);
 
-  return (
-    <div>
-      <h2> Eliminar {name}</h2>
-      <p>Por favor, escribe el codigo de seguridad</p>
-      {error && !loading && <p> Error : codigo incorrecto</p>}
-      {loading && <p> Cargando...</p>}
-      <input
-        placeholder="codigo de seguridad"
-        value={value}
-        onChange={(event) => {
-          setValue(event.target.value);
-        }}
-      />
-      <button onClick={() => setLoading(true)}>Comprobar</button>
-    </div>
-  );
+  if (!state.deleted && !state.confirmed) {
+    return (
+      <div>
+        <h2>Eliminar {name}</h2>
+
+        <p>Por favor, escribe el código de seguridad.</p>
+
+        {state.error && !state.loading && <p>Error: el código es incorrecto</p>}
+        {state.loading && <p>Cargando...</p>}
+
+        <input
+          placeholder="Código de seguridad"
+          value={state.value}
+          onChange={(event) => {
+            setState({
+              ...state,
+              value: event.target.value,
+            });
+          }}
+        />
+        <button
+          onClick={() => {
+            setState({
+              ...state,
+              loading: true,
+            });
+          }}
+        >
+          Comprobar
+        </button>
+      </div>
+    );
+  } else if (!!state.confirmed && !state.deleted) {
+    return (
+      <React.Fragment>
+        <p>Pedimos confirmación. ¿Tas segurx?</p>
+        <button
+          onClick={() => {
+            setState({
+              ...state,
+              deleted: true,
+            });
+          }}
+        >
+          Sí, eliminar
+        </button>
+        <button
+          onClick={() => {
+            setState({
+              ...state,
+              confirmed: false,
+              value: "",
+            });
+          }}
+        >
+          Nop, me arrepentí
+        </button>
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <p>Eliminado con éxito</p>
+        <button
+          onClick={() => {
+            setState({
+              ...state,
+              confirmed: false,
+              deleted: false,
+              value: "",
+            });
+          }}
+        >
+          Resetear, volver atrás
+        </button>
+      </React.Fragment>
+    );
+  }
 }
